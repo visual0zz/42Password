85f969855afbf8853ca2e07b18d2872ab47a27a2
zhaozhi_bjb <indiancarriernoac@qq.com>
2019-11-20 16:09:30 +0800

    ..

---
+++ app/src/main/java/com/zz/notebook/git/RepoManager.java
@@ -0,0 +1,23 @@
+package com.zz.notebook.git;
+
+import java.io.File;
+
+public class RepoManager {
+    RepoManager(String folderPath){
+
+    }
+    public void push(String remote,String branch){
+
+    }
+
+    public void clone(String remote){
+
+    }
+
+    public void init(){
+
+    }
+    public void putFile(String file){
+
+    }
+}
